f4346a55fae39ab6cb1d65af5d42e287846c7890
NaveenKomara07 <135030450+NaveenKomara07@users.noreply.github.com>
2023-07-22 07:02:30 +0530

    Update index.js

--- src/components/GradientDirectionItem/index.js
+++ src/components/GradientDirectionItem/index.js
@@ -2,8 +2,8 @@
 import {ListItem, DirectionButton} from './styledComponents'
 
 const GradientDirectionItem = props => {
-  const {GradientDirectionDetails, isActive, clickGradientDirectionItem} = props
-  const {displayText, value} = GradientDirectionDetails
+  const {gradientDirectionDetails, isActive, clickGradientDirectionItem} = props
+  const {displayText, value} = gradientDirectionDetails
 
   const onClickGradientDirectionItem = () => {
     clickGradientDirectionItem(value)
